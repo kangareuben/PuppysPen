66c74ebc8490aac44999558e62d72a39bada52b2
Will Paul <whp3652@rit.edu>
2014-11-18 07:41:09 -0500

    Fix constructor parameter name, experiment with pygame surface

--- PuppysPen.py
+++ PuppysPen.py
@@ -18,17 +18,18 @@ from pygame.locals import QUIT, MOUSEBUTTONUP, MOUSEMOTION, VIDEORESIZE, ACTIVEE
 class PuppysPen:
 
     # Runs before the game loop begins
-    def __init__(self):
+    def __init__(self, _screen):
         self.running = True
+        self.screen = _screen;
 
     def draw_rectangle(self, _x, _y, _width, _height):
         pass
 
     def draw_grid(self, _width, _height, _num_rows, _num_columns):
-        _row_height = int(height / num_rows)
-        _column_width = int(width / num_columns)
+        _row_height = int(_height / _num_rows)
+        _column_width = int(_width / _num_columns)
 
-        for x in range(0, num_columns + 1):
+        for x in range(0, _num_columns + 1):
             draw_rectangle(x * _column_width, 0, 1, _height)
 
         for y in range(0, _num_rows + 1):
@@ -51,8 +52,9 @@ class PuppysPen:
 # ./PuppysPen.py
 def main():
     pygame.init()
-    pygame.display.set_mode((1200, 900 - 54), pygame.RESIZABLE) # 54 = height of sugar toolbar
-    game = PuppysPen()
+    screen = pygame.display.set_mode((1200, 900 - 54), pygame.RESIZABLE) # 54 = height of sugar toolbar
+    screen.fill((255,255,255))
+    game = PuppysPen(screen)
     game.run()
 
 if __name__ == '__main__':
